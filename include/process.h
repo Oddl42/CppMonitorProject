47faead17822bbb6dff3f791517a33b43fe2f2e2
Oddl42 <94608855+Oddl42@users.noreply.github.com>
2021-11-18 11:49:12 +0100

    Update process.h

--- include/process.h
+++ include/process.h
@@ -2,22 +2,30 @@
 #define PROCESS_H
 
 #include <string>
-/*
-Basic class for Process representation
-It contains relevant attributes as shown below
-*/
+
+#include "linux_parser.h"
+#include "format.h"
+
 class Process {
  public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  Process(int pid){
+      pid_ = pid;
+      cpuUtilization_ = Process::CpuUtilization();
+  }
+  int Pid();                               
+  std::string User();                      
+  std::string Command();                   
+  float CpuUtilization();                  
+  std::string Ram();                      
+  long int UpTime();                       
+  bool operator<(Process const& a) const;
 
-  // TODO: Declare any necessary private members
  private:
+   int pid_;
+   std::string ram_;
+   std::string command_;
+   long upTime_;
+   std::string user_;
+   float cpuUtilization_;
 };
-
 #endif
